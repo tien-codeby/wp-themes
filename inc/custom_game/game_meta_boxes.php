6a36aa7c8c48f9cc763dfd896effea25b14cc82f
tien.luckyfor <tien.luckyfor@gmail.com>
2022-04-30 17:05:16 +0700

    list home

--- inc/custom_game/game_meta_boxes.php
+++ inc/custom_game/game_meta_boxes.php
@@ -24,27 +24,27 @@ function custom_game_meta_boxes( $meta_boxes ) {
                 'name' => esc_html__( 'Fanpage Hỗ Trợ', 'online-generator' ),
                 'id'   => $prefix . 'fanpage_ho_tro',
             ],
-            [
-                'type'    => 'select',
-                'name'    => esc_html__( 'Phiên Bản', 'online-generator' ),
-                'id'      => $prefix . 'phien_ban',
-                'options' => [
-                    'Season 2'  => esc_html__( 'Season 2', 'online-generator' ),
-                    'Season 6'  => esc_html__( 'Season 6', 'online-generator' ),
-                    'Season 16' => esc_html__( 'Season 16', 'online-generator' ),
-                ],
-                'std'     => 'Season 2',
-            ],
-            [
-                'type'    => 'radio',
-                'name'    => esc_html__( 'Loại Mu', 'online-generator' ),
-                'id'      => $prefix . 'loai_mu',
-                'options' => [
-                    'Reset'     => esc_html__( 'Reset', 'online-generator' ),
-                    'Non Reset' => esc_html__( 'Non Reset', 'online-generator' ),
-                ],
-                'std'     => 'Reset',
-            ],
+//            [
+//                'type'    => 'select',
+//                'name'    => esc_html__( 'Phiên Bản', 'online-generator' ),
+//                'id'      => $prefix . 'phien_ban',
+//                'options' => [
+//                    'Season 2'  => esc_html__( 'Season 2', 'online-generator' ),
+//                    'Season 6'  => esc_html__( 'Season 6', 'online-generator' ),
+//                    'Season 16' => esc_html__( 'Season 16', 'online-generator' ),
+//                ],
+//                'std'     => 'Season 2',
+//            ],
+//            [
+//                'type'    => 'radio',
+//                'name'    => esc_html__( 'Loại Mu', 'online-generator' ),
+//                'id'      => $prefix . 'loai_mu',
+//                'options' => [
+//                    'Reset'     => esc_html__( 'Reset', 'online-generator' ),
+//                    'Non Reset' => esc_html__( 'Non Reset', 'online-generator' ),
+//                ],
+//                'std'     => 'Reset',
+//            ],
             [
                 'type' => 'text',
                 'name' => esc_html__( 'Tên Máy Chủ', 'online-generator' ),
@@ -80,16 +80,16 @@ function custom_game_meta_boxes( $meta_boxes ) {
                 'name' => esc_html__( 'Anti Hack', 'online-generator' ),
                 'id'   => $prefix . 'anti_hack',
             ],
-            [
-                'type'    => 'radio',
-                'name'    => esc_html__( 'Loại bài', 'online-generator' ),
-                'id'      => $prefix . 'loai_bai',
-                'options' => [
-                    'Thường' => esc_html__( 'Thường', 'online-generator' ),
-                    'VIP'       => esc_html__( 'VIP', 'online-generator' ),
-                ],
-                'std'     => 'Thường',
-            ],
+//            [
+//                'type'    => 'radio',
+//                'name'    => esc_html__( 'Loại bài', 'online-generator' ),
+//                'id'      => $prefix . 'loai_bai',
+//                'options' => [
+//                    'Thường' => esc_html__( 'Thường', 'online-generator' ),
+//                    'VIP'       => esc_html__( 'VIP', 'online-generator' ),
+//                ],
+//                'std'     => 'Thường',
+//            ],
         ],
     ];
 
